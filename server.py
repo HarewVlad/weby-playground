df00e2f4aa43158cad04abdd4934139ffce2d980
Vladislav Petrenko <Harewvlad@mail.ru>
2025-06-24 22:58:39 +0200

    Update

--- server.py
+++ server.py
@@ -567,7 +567,7 @@ async def weby(
                     temperature=request.temperature,
                     top_p=request.top_p,
                     extra_body={
-                        "provider": {"order": ["Baseten"], "allow_fallbacks": False}
+                        "provider": {"order": ["baseten/fp8"], "allow_fallbacks": False}
                     },
                 )
 
